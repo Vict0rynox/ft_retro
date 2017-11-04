6338a1aad40b11067dc3178f86ac086b2b25cc82
vvasilie <victorsecuring@gmail.com>
2017-11-04 20:58:55 +0200

    add Controlle

---
+++ src/view/ObjectViewer.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Victor Vasiliev on 11/4/17.
+//
+
+#include "ObjectViewer.hpp"
+
+void View::ObjectViewer::view(Model::INcursesView *view)
+{
+	view->getView();
+}
---
+++ src/view/ObjectViewer.hpp
@@ -0,0 +1,19 @@
+//
+// Created by Victor Vasiliev on 11/4/17.
+//
+
+#ifndef FT_RETRO_VIEWER_HPP
+#define FT_RETRO_VIEWER_HPP
+
+
+#include "../model/INcursesView.hpp"
+
+namespace View {
+	class ObjectViewer {
+
+	public:
+		void view(Model::INcursesView *view);
+	};
+}
+
+#endif //FT_RETRO_VIEWER_HPP
