2c817b2324c63a5c5d782c2ac30552492bfad996
vvasilie <victorsecuring@gmail.com>
2017-11-05 02:09:26 +0200

    add fps

---
+++ src/view/FPSViewer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Victor Vasiliev on 11/5/17.
+//
+
+#include "FPSViewer.hpp"
---
+++ src/view/FPSViewer.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Victor Vasiliev on 11/5/17.
+//
+
+#ifndef FT_RETRO_FPSVIEWER_HPP
+#define FT_RETRO_FPSVIEWER_HPP
+
+
+class FPSViewer {
+
+};
+
+
+#endif //FT_RETRO_FPSVIEWER_HPP
